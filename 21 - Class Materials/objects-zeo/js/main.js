64ba09a0346fd91f9736d8e3f403ad6171f00350
dom <35171942+domluther@users.noreply.github.com>
2024-06-08 21:14:11 +0100

    100 devs time

--- 21 - Class Materials/objects-zeo/js/main.js
+++ 21 - Class Materials/objects-zeo/js/main.js
@@ -1,1 +1,23 @@
 //Create a stopwatch object that has four properties and three methods
+
+const stopwatch = {
+  colour: 'black',
+  minutes: 5,
+  seconds: 20,
+  brand: 'casio',
+  describeSelf() {
+    console.log(`I am a ${this.colour} stopwatch`);
+  },
+  reset() {
+    this.minutes = 0;
+    this.seconds = 0;
+  },
+  sayTime() {
+    console.log(`Current time: ${this.minutes}:${this.seconds}`);
+  },
+};
+
+stopwatch.describeSelf();
+stopwatch.sayTime();
+stopwatch.reset();
+stopwatch.sayTime();
