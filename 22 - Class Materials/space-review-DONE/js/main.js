7b2f350830d092286e1c96d71e1396f180422bdd
dom <35171942+domluther@users.noreply.github.com>
2024-06-12 06:24:07 +0100

    100 devs time

---
+++ 22 - Class Materials/space-review-DONE/js/main.js
@@ -0,0 +1,34 @@
+//Arrays
+
+//Create and array of numbers. Sum all of the numbers. Alert the sum.
+nums = [1, 2, 3, 4, 5, 6];
+sum = nums.reduce((acc, cur) => acc + cur);
+
+console.log(sum);
+
+//Create a function that takes in an array of numbers
+//Return a new array of numbers that is every original number squared
+function squareArr(arrIn) {
+  return arrIn.map((val) => val * val);
+}
+
+const ans = squareArr(nums);
+console.log(ans);
+
+//Create a function that takes string
+//Print the reverse of that string to the console
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString('hello'));
+
+//Create a function that takes in a string
+//Alert if the string is a palindrome or not
+
+function isPalindrome(str) {
+  return str === reverseString(str);
+}
+
+const pal = isPalindrome('hih');
+console.log(pal);
