2e778e6fe97370d0999a2aaf2958bdf3f7854765
dom <35171942+domluther@users.noreply.github.com>
2024-06-12 20:22:32 +0100

    100 devs time

--- 22 - Class Materials/objects-lost-galaxy-DONE/js/main.js
+++ 22 - Class Materials/objects-lost-galaxy-DONE/js/main.js
@@ -43,3 +43,31 @@ const computerMouse = {
 computerMouse.leftClick();
 computerMouse.rightClick();
 computerMouse.wiggle();
+
+function Mouse(name, colour, legs = 4) {
+  this.name = name;
+  this.colour = colour;
+  this.legs = legs;
+  this.sayHi = function () {
+    console.log(
+      `I am ${this.name} the ${this.colour} mouse with ${this.legs} legs`
+    );
+  };
+  this.growLeg = function (num) {
+    this.legs += num;
+  };
+  this.squeak = function () {
+    console.log('SQUEAK!');
+  };
+}
+
+Mouse.prototype.favouriteFood = 'cheese';
+
+const mickey = new Mouse('Mickey', 'white');
+const minnie = new Mouse('Minnie', 'pink');
+
+mickey.sayHi();
+minnie.growLeg(2);
+minnie.sayHi();
+mickey.sayHi();
+console.log(mickey.favouriteFood);
--- 22 - Class Materials/objects-street-fighter/js/main.js
+++ 22 - Class Materials/objects-street-fighter/js/main.js
@@ -1,1 +1,28 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighter(name, aAttack, bAttack, specialMove) {
+  this.name = name;
+  this.aAttack = aAttack;
+  this.bAttack = bAttack;
+  this.specialMove = specialMove;
+  this.aPushed = () => {
+    console.log(`${this.name} uses ${this.aAttack}`);
+  };
+  this.bPushed = () => {
+    console.log(`${this.name} uses ${this.bAttack}`);
+  };
+  this.useSpecial = () => {
+    console.log(`${this.name} uses ${this.specialMove} and wins!`);
+  };
+}
+
+char1 = new StreetFighter('Ken', 'punch', 'kick', 'fireball');
+char2 = new StreetFighter('Chun-Li', 'slap', 'headbutt', 'flying kick');
+
+char1.aPushed();
+char1.bPushed();
+char1.useSpecial();
+
+char2.aPushed();
+char2.bPushed();
+char2.useSpecial();
