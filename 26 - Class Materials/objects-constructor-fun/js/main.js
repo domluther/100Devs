568b37b8ffbafdd351da83ef3711fb5e94171ab6
dom <35171942+domluther@users.noreply.github.com>
2024-06-17 20:41:44 +0100

    100 devs - object practice pizzas factory

--- 26 - Class Materials/objects-constructor-fun/js/main.js
+++ 26 - Class Materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,23 @@
 //Create a constructor with 4 properties and 3 methods
+
+const first = require('eslint-plugin-import/lib/rules/first');
+
+function Pizza(topping, inches, crust) {
+  this.topping = topping;
+  this.inches = inches;
+  this.crust = crust;
+  this.area = Math.pow(this.inches, 2) * Math.PI;
+
+  this.deliver = function () {
+    console.log('delivering');
+  };
+  this.frisbee = function () {
+    console.log('frisbeed away');
+  };
+}
+
+const firstPizza = new Pizza('cheese', 12, 'thin');
+
+console.log(firstPizza);
+firstPizza.deliver();
+firstPizza.frisbee();
