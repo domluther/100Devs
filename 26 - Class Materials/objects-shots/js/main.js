6a63663baa2b4493ee1084cd4b1543e128cdba58
dom <35171942+domluther@users.noreply.github.com>
2024-06-18 21:47:46 +0100

    100 devs - carousel cocktail

--- 26 - Class Materials/objects-shots/js/main.js
+++ 26 - Class Materials/objects-shots/js/main.js
@@ -1,8 +1,14 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-const buttonEle = document.querySelector('button');
+const buttonEle = document.querySelector('#getButton');
 buttonEle.addEventListener('click', fetchCocktail);
 
+document.querySelector('#incrementButton').addEventListener('click', increment);
+document.querySelector('#decrementButton').addEventListener('click', decrement);
+document.querySelector('#decrementButton').disabled = true;
+
+let fetchedData = [];
+let counter = 0;
 function fetchCocktail(e) {
   e.preventDefault();
   // Read in the value
@@ -12,19 +18,37 @@ function fetchCocktail(e) {
   // Fetch
   fetch(url)
     .then((res) => res.json())
-    .then((data) => parseData(data));
+    .then((data) => {
+      fetchedData = data.drinks;
+      counter = 0;
+      parseData();
+    });
 }
 
-function parseData(data) {
-  // Destructure the first drink
-  const [first] = [...data.drinks];
-  console.log(first);
+function parseData() {
+  const drink = fetchedData[counter];
   //   Update DOM based on fetched data
-  document.querySelector('img').src = first.strDrinkThumb;
-  document.querySelector('img').alt = first.strDrink;
-  document.querySelector('h2').innerText = first.strDrink;
-  document.querySelector('h3').innerText = first.strInstructions.replaceAll(
+  document.querySelector('img').src = drink.strDrinkThumb;
+  document.querySelector('img').alt = drink.strDrink;
+  document.querySelector('h2').innerText = drink.strDrink;
+  document.querySelector('h3').innerText = drink.strInstructions.replaceAll(
     '.',
     '\n'
   );
+}
+
+function increment() {
+  counter++;
+  parseData();
+  document.querySelector('#decrementButton').disabled = false;
+  // Get to the end? Disable the button
+  if (counter + 1 > fetchedData.length - 1)
+    document.querySelector('#incrementButton').disabled = true;
+}
+
+function decrement() {
+  counter--;
+  parseData();
+  document.querySelector('#incrementButton').disabled = false;
+  if (counter === 0) document.querySelector('#decrementButton').disabled = true;
 }
