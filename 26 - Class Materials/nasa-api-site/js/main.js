3f52f33873f7fc0c946040bb7f5321c5c0a3d4a6
dom <35171942+domluther@users.noreply.github.com>
2024-06-18 20:16:00 +0100

    100 devs - nasa api

--- 26 - Class Materials/nasa-api-site/js/main.js
+++ 26 - Class Materials/nasa-api-site/js/main.js
@@ -2,9 +2,11 @@
 document.querySelector('button').addEventListener('click', getFetch);
 
 function getFetch() {
-  const choice = document.querySelector('input').value;
-  const url = 'https://pokeapi.co/api/v2/pokemon/' + choice;
+  // const choice = document.querySelector('input').value
+  // Not working when tested 18th June - down?
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=MYKEY';
 
+  console.log('hi');
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
