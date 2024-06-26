d5a67ae8977a927e40270395cc85f7c76c1032ff
dom <35171942+domluther@users.noreply.github.com>
2024-06-26 21:51:51 +0100

    local storage

--- 27 - Class Materials/local-storage-botScore-button/js/main.js
+++ 27 - Class Materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,17 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+//Create a button that adds 1 to a botScore stored in localStorage
+if (!localStorage.getItem('botScore')) localStorage.setItem('botScore', 0);
+displayScore();
+document.querySelector('#increase').addEventListener('click', increaseScore);
+document
+  .querySelector('#increase')
+  .addEventListener('click', localStorage.clear());
 
+function increaseScore() {
+  localStorage.setItem('botScore', +localStorage.getItem('botScore') + 1);
+  displayScore();
+}
 
+function displayScore() {
+  document.querySelector('h2').innerText =
+    `Score: ${localStorage.getItem('botScore')}`;
+}
