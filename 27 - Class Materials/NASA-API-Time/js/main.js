2866cd856ed2487687b15b3228258b7d09cfc160
dom <35171942+domluther@users.noreply.github.com>
2024-06-26 20:02:05 +0100

    nasa api

---
+++ 27 - Class Materials/NASA-API-Time/js/main.js
@@ -0,0 +1,19 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch);
+
+function getFetch() {
+  const date = document.querySelector('input').value;
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
+
+  fetch(url)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      document.querySelector('#title').innerText = data.title;
+      document.querySelector('#explanation').innerText = data.explanation;
+      document.querySelector('#potd').src = data.url;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
