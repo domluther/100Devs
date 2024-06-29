ec5bfb94a2446468f6e914062623c637073965a8
dom <35171942+domluther@users.noreply.github.com>
2024-06-29 13:21:50 +0100

    started dnd api code

--- 28 - Class Materials/dnd/js/main.js
+++ 28 - Class Materials/dnd/js/main.js
@@ -1,17 +1,24 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+function getFetch() {
+  const choice = document.querySelector('input').value;
+  const url = `https://www.dnd5eapi.co/api/spells/${choice}`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      document.querySelector('h2').innerText = data.name;
+      const usableClasses = data.classes;
+      for (usableClass of usableClasses) {
+        document.querySelector('h3').innerText += usableClass.name;
+      }
+      const subClasses = data.subclasses;
+      for (subClass of subClasses) {
+        document.querySelector('h4').innerText += usableClass.name;
+      }
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
-
